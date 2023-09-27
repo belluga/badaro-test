9320f51d563a035079af903ebb7c159677bf2ca4
eltonmorais <elton@marketinglucrativo.com>
2023-09-27 12:33:02 -0300

    :bricks: DTO to Domain functions

--- lib/infrastructure/dal/dao/message_dto.dart
+++ lib/infrastructure/dal/dao/message_dto.dart
@@ -1,3 +1,5 @@
+import 'package:badaro_test/domain/models/message_model.dart';
+import 'package:badaro_test/domain/value_objects/message/message_content_value.dart';
 import 'package:badaro_test/infrastructure/dal/dao/chat_agent_dto.dart';
 import 'package:badaro_test/infrastructure/dal/dao/user_dto.dart';
 
@@ -14,4 +16,25 @@ class MessageDTO {
           chatAgentDTO != null || userDTO != null,
           "Either userDTO or chatAgentDTO shouldn't be null.",
         );
+
+  MessageModel toDomain() {
+    return MessageModel(
+      contentValue: MessageContentValue()..parse(content),
+      chatAgent: chatAgentDTO?.toDomain(),
+      user: userDTO?.toDomain(),
+    );
+  }
+
+  factory MessageDTO.fromJson(Map<String, dynamic> json) {
+    final String _content = json["content"];
+    final ChatAgentDTO? _chatAgentDTO =
+        ChatAgentDTO.fromJsonTry(json["chat_agent"]);
+    final UserDTO? _userDTO = UserDTO.fromJsonTry(json["user"]);
+
+    return MessageDTO(
+      content: _content,
+      chatAgentDTO: _chatAgentDTO,
+      userDTO: _userDTO,
+    );
+  }
 }
