542591ed49e1eef557bb4035910cc3af4db8d738
eltonmorais <elton@marketinglucrativo.com>
2023-09-27 10:21:43 -0300

    project created with folder structure

---
+++ android/app/src/main/kotlin/com/example/badaro_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.badaro_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:badaro_test/application/application.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
